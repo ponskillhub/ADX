56356cab4b8ad155f5119b7d335e409d2d5a052f
ponjagannath <ponskillhub@gmail.com>
2023-06-09 15:26:23 +0530

    hi

---
+++ concurrencyTest/config.py
@@ -0,0 +1,6 @@
+config = {
+    'tenant_id': 'c95af50c-5739-4b14-b695-c86cb211e08e',
+    'cluster': 'https://ponsandboxadxtest1.eastus.kusto.windows.net',
+    'database': 'rubrikadxpoc',
+    'number_of_threads': 40
+}
